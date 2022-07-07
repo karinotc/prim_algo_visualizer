a06c6f908036819d637f00fe69967bc51ac07e6f
Kurtova Karina <karinotc@gmail.com>
2022-07-07 14:16:59 +0300

    Added reading graph from file

---
+++ src/main/java/edu/app/input/GraphReader.java
@@ -0,0 +1,40 @@
+package edu.app.input;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import com.google.gson.Gson;
+import edu.app.graph.WeightedConnectedGraph;
+
+public class GraphReader {
+    public GraphReader() {
+    }
+
+    public static WeightedConnectedGraph readFromJSON() {
+        WeightedConnectedGraph graph = new WeightedConnectedGraph();
+        String filePath = new File("").getAbsolutePath().concat("\\graph.json");
+
+        Gson gson = new Gson();
+        try (Reader reader = new FileReader(filePath)) {
+            GraphInfo info = gson.fromJson(reader, GraphInfo.class);
+
+            for (int i = 0; i < info.nodeNumber; i++) {
+                graph.addNode();
+            }
+
+            for (int i = 0; i < info.edges.length; i++) {
+                graph.addWeightedEdge(info.edges[i][0], info.edges[i][1], info.edges[i][2]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return graph;
+    }
+
+    private static class GraphInfo {
+        public int nodeNumber = 0;
+        public int[][] edges;
+    }
+}
